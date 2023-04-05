6e7f2e48626f56309be821cf874fa9da351d2724
dung <vhngdng@gmail.com>
2023-04-06 01:46:49 +0700

    fix dispatch

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ cd web-bid
 
 if [ ! -d config ] 
 then
-  rsync -ravh ~/Documents/web/config $USER@$DIGITALOCEAN_DROPLET_IP:./config
+  rsync -ravh config $USER@$DIGITALOCEAN_DROPLET_IP:./config
 fi
 
 dockerCompose="./docker-compose.yml"
 if [ -e "$dockerCompose" ] 
 then 
   rm "$dockerCompose"
-  rsync -ravh ~/Documents/web/docker-compose.yml $USER@$DIGITALOCEAN_DROPLET_IP:./docker-compose.yml
+  rsync -ravh docker-compose.yml $USER@$DIGITALOCEAN_DROPLET_IP:./docker-compose.yml
 fi
 
-[ ! -e .env ] && rsync -ravh ~/Documents/web/.env $USER@$DIGITALOCEAN_DROPLET_IP:./.env
+[ ! -e .env ] && rsync -ravh .env $USER@$DIGITALOCEAN_DROPLET_IP:./.env
 
 docker compose up -d --build
 
