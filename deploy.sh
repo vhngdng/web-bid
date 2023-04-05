414d63fedeed687df82b65cb1a2f4c8fbfc081f4
dung <vhngdng@gmail.com>
2023-04-05 21:00:08 +0700

    fix deploy stage

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@ cd web-bid
 
 if [ ! -d config ] 
 then
-  scp -r ./config dung@$DIGITALOCEAN_DROPLET_IP:./config
+  scp -r ./config $USER@$DIGITALOCEAN_DROPLET_IP:./config
 fi
 
 dockerCompose="./docker-compose.yml"
 if [ -e "$dockerCompose" ] 
 then 
   rm "$dockerCompose"
-  scp ./docker-compose.yml dung@$DIGITALOCEAN_DROPLET_IP:./docker-compose.yml
+  scp ./docker-compose.yml $USER@$DIGITALOCEAN_DROPLET_IP:./docker-compose.yml
 fi
 
-[ ! -e .env ] && scp ./.env dung@$DIGITALOCEAN_DROPLET_IP:./.env
+[ ! -e .env ] && scp ./.env $USER@$DIGITALOCEAN_DROPLET_IP:./.env
 
 docker compose up -d --build
 
