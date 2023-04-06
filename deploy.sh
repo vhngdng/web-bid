1f519cd6b475430f32cd9388cfb6028c8f431b51
dung <vhngdng@gmail.com>
2023-04-06 11:52:26 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 
-
-
-
-dockerCompose="./docker-compose.yml"
-if [ -e "$dockerCompose" ] 
-then 
-  rm "$dockerCompose"
-  rsync -ravh docker-compose.yml $USER@$DIGITALOCEAN_DROPLET_IP:./docker-compose.yml
-fi
-
-[ ! -e .env ] && rsync -ravh .env $USER@$DIGITALOCEAN_DROPLET_IP:./.env
+echo "Hello World"
 
 docker compose up -d --build
 
