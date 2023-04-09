18a9e645ead04c2f117b49cb2deee8d454fbc35f
dung <vhngdng@gmail.com>
2023-04-10 02:30:26 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 
 echo "Hello World"
-docker compose stop
 docker pull vuhoangdung/web-bid-frontend:latest && docker pull vuhoangdung/web-bid-backend:latest
 docker rmi $(docker images -qa -f 'dangling=true')
 docker-compose up -d --no-deps --build postgre
