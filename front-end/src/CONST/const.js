b5aa81a45cc0ac3703588db1038787c83c9e07dd
dung <vhngdng@gmail.com>
2023-04-10 14:28:23 +0700

    fix port in api front end

--- front-end/src/CONST/const.js
+++ front-end/src/CONST/const.js
@@ -17,5 +17,5 @@ export const GITHUB_AUTH_URL =
     OAUTH2_REDIRECT_URI;
 export const AUTHPUBLIC = 'authBlog';
 
-export const DOMAIN_URL = 'http://auctionforfun.site:443/';
-export const DOMAIN_BACKEND_WWS = 'http://auctionforfun.site:8080/';
+export const DOMAIN_URL = 'http://auctionforfun.site/';
+export const DOMAIN_BACKEND_WWS = 'http://auctionforfun.site/';
