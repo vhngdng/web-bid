57c5c57aef8e474e51a83aceff637489845a5c6c
dung <vhngdng@gmail.com>
2023-04-11 11:46:08 +0700

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,17 @@
 
 
 echo "Hello World"
+export PROPERTY=${PROPERTY:-dev}
+if [ ! "${PROPERTY}" = 'dev' ] 
+then 
+  export NGINX_PORT=80 
+else
+  export NGINX_PORT=8081
+fi
+
 docker pull vuhoangdung/web-bid-frontend:latest && docker pull vuhoangdung/web-bid-backend:latest
+docker compose stop
 docker rmi $(docker images -qa -f 'dangling=true')
-docker compose up -d --build --no-deps backend frontend nginx
+docker compose up -d --build --force-recreate --no-deps backend frontend nginx
 exit
 
