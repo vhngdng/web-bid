a9d624f0dc32aa3444d4bbfed322ebeccb85895a
dung <vhngdng@gmail.com>
2023-04-11 16:23:21 +0700

    push

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,10 @@ then
   echo 'REACT_APP_DOMAIN_URL=https://auctionforfun.site/' >> .env
   echo 'NGINX_CONFIG_PART=./config/nginx.conf' >> .env
 else
-  echo 'PROPERTY=local' >> .env
-  echo 'NGINX_PORT=8081' >> .env
-  echo 'REACT_APP_DOMAIN_URL=http://localhost:8380/' >> .env
-  echo 'NGINX_CONFIG_PART=./config/nginx.local.conf' >> .env
+  ( ! cat .env | grep "PROPERTY" ) && echo "PROPERTY=local" >> .env
+  ( ! cat .env | grep "NGINX_PORT" ) && echo 'NGINX_PORT=8081' >> .env
+  ( ! cat .env | grep "REACT_APP_DOMAIN_URL" ) && echo 'REACT_APP_DOMAIN_URL=http://localhost:8080/' >> .env
+  ( ! cat .env | grep "NGINX_CONFIG_PART" ) && echo 'NGINX_CONFIG_PART=./config/nginx.local.conf' >> .env
 fi
 
 docker pull vuhoangdung/web-bid-frontend:latest && docker pull vuhoangdung/web-bid-backend:latest
