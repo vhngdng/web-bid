fccbc4d25722a7203e36ac6efe13cf6c7f81e063
dung <vhngdng@gmail.com>
2023-04-11 12:17:34 +0700

    fix config nginx path

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,13 @@ if [ ! "${PROPERTY}" = 'dev' ]
 then 
   export NGINX_PORT=80
   export REACT_APP_DOMAIN_URL=https://auctionforfun.site/ 
+  export NGINX_CONFIG_PART=./config/nginx.conf 
+
 else
   export NGINX_PORT=8081
   export REACT_APP_DOMAIN_URL=http://localhost:8080/
+  export NGINX_CONFIG_PART=./config/nginx.local.conf 
+
 fi
 
 docker pull vuhoangdung/web-bid-frontend:latest && docker pull vuhoangdung/web-bid-backend:latest
