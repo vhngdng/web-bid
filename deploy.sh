f8623d7d04d692188e476fc92062ad029aefa71a
dung <vhngdng@gmail.com>
2023-04-11 12:42:12 +0700

    fix log nginx

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,24 @@
 
 
 echo "Hello World"
+
 export PROPERTY=${PROPERTY:-dev}
 if [ ! "${PROPERTY}" = 'dev' ] 
 then 
   export NGINX_PORT=80
   export REACT_APP_DOMAIN_URL=https://auctionforfun.site/ 
   export NGINX_CONFIG_PART=./config/nginx.conf 
-
 else
   export NGINX_PORT=8081
   export REACT_APP_DOMAIN_URL=http://localhost:8080/
   export NGINX_CONFIG_PART=./config/nginx.local.conf 
-
 fi
 
 docker pull vuhoangdung/web-bid-frontend:latest && docker pull vuhoangdung/web-bid-backend:latest
 docker compose stop
 docker rmi $(docker images -qa -f 'dangling=true')
 docker compose up -d --build --force-recreate --no-deps backend frontend nginx
+docker compose up -d --build --no-deps postgre
+
 exit
 
