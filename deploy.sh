fe5d203ec9496cd19fa6df6bf536db5ceca42662
dung <vhngdng@gmail.com>
2023-04-11 12:45:10 +0700

    fix .env

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ echo "Hello World"
 export PROPERTY=${PROPERTY:-dev}
 if [ ! "${PROPERTY}" = 'dev' ] 
 then 
-  export NGINX_PORT=80
-  export REACT_APP_DOMAIN_URL=https://auctionforfun.site/ 
-  export NGINX_CONFIG_PART=./config/nginx.conf 
+  echo 'NGINX_PORT=80' >> .env
+  echo 'REACT_APP_DOMAIN_URL=https://auctionforfun.site/' >> .env
+  echo 'NGINX_CONFIG_PART=./config/nginx.conf' >> .env
 else
-  export NGINX_PORT=8081
-  export REACT_APP_DOMAIN_URL=http://localhost:8080/
-  export NGINX_CONFIG_PART=./config/nginx.local.conf 
+  echo 'NGINX_PORT=8081' >> .env
+  echo 'REACT_APP_DOMAIN_URL=http://localhost:8080/' >> .env
+  echo 'NGINX_CONFIG_PART=./config/nginx.local.conf' >> .env
 fi
 
 docker pull vuhoangdung/web-bid-frontend:latest && docker pull vuhoangdung/web-bid-backend:latest
