143496ca51b57104feb78f2e18373a15a7da18d1
dung <vhngdng@gmail.com>
2023-04-11 16:08:19 +0700

    fix .env

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,15 @@ echo "Hello World"
 export PROPERTY=${PROPERTY:-dev}
 if [ "${PROPERTY}" = 'dev' ] 
 then 
-  [ ! -z ${PROPERTY} ] && echo 'PROPERTY=dev' >> .env
-  [ ! -z ${NGINX_PORT} ] && echo 'NGINX_PORT=80' >> .env
-  [ ! -z ${REACT_APP_DOMAIN_URL} ] && echo 'REACT_APP_DOMAIN_URL=https://auctionforfun.site/' >> .env
-  [ ! -z ${NGINX_CONFIG_PART} ] && echo 'NGINX_CONFIG_PART=./config/nginx.conf' >> .env
+  echo 'PROPERTY=dev' >> .env
+  echo 'NGINX_PORT=80' >> .env
+  echo 'REACT_APP_DOMAIN_URL=https://auctionforfun.site/' >> .env
+  echo 'NGINX_CONFIG_PART=./config/nginx.conf' >> .env
 else
-  [ ! -z ${PROPERTY} ] && echo 'PROPERTY=local' >> .env
-  [ ! -z ${NGINX_PORT} ] && echo 'NGINX_PORT=8081' >> .env
-  [ ! -z ${REACT_APP_DOMAIN_URL} ] && echo 'REACT_APP_DOMAIN_URL=http://localhost:8380/' >> .env
-  [ ! -z ${NGINX_CONFIG_PART} ] && echo 'NGINX_CONFIG_PART=./config/nginx.local.conf' >> .env
+  echo 'PROPERTY=local' >> .env
+  echo 'NGINX_PORT=8081' >> .env
+  echo 'REACT_APP_DOMAIN_URL=http://localhost:8380/' >> .env
+  echo 'NGINX_CONFIG_PART=./config/nginx.local.conf' >> .env
 fi
 
 docker pull vuhoangdung/web-bid-frontend:latest && docker pull vuhoangdung/web-bid-backend:latest
