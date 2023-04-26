74c668fa2f265ed55e994c3699e71f25e4d5a837
dung <vhngdng@gmail.com>
2023-04-26 12:00:29 +0700

    fix UI menu

--- front-end/src/page/HomePage/property/ImageSlideShow.js
+++ front-end/src/page/HomePage/property/ImageSlideShow.js
@@ -11,7 +11,7 @@ function ImageSlideShow({ images }) {
     return (
         <div className="w-full my-5">
             <div className="flex justify-center items-center">
-                <div className="w-30vw h-50vh p-3 bg-white/30 rounded-lg">
+                <div className="w-30vw h-50vh p-3 bg-white/30 p-3">
                     <img
                         className="w-full h-full object-cover rounded-lg shadow-[0_50px_25px_-24px_rgb(0,0,0,0.3)]"
                         src={
@@ -22,15 +22,11 @@ function ImageSlideShow({ images }) {
                     />
                 </div>
             </div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap mt-7">
                 {images.length > 1 &&
                     images.map((image, index) => (
                         <div
-                            key={index}
-                            className="w-48 bg-white/30 h-48 rounded-lg m-3 p-3"
-                        >
-                            <div
-                                className={`cursor-pointer w-full h-full
+                            className={`w-48 bg-white/30 h-48 rounded-lg m-3 p-3
                                                                 ${
                                                                     images.indexOf(
                                                                         image,
@@ -39,15 +35,13 @@ function ImageSlideShow({ images }) {
                                                                         ? 'ring-offset-2 ring-blue-300 ring-2 shadow-[0_50px_25px_-24px_rgb(0,0,0,0.3)] rounded-lg'
                                                                         : 'contrast-50'
                                                                 }`}
-                                onClick={() =>
-                                    setIndexImage(images.indexOf(image))
-                                }
-                            >
-                                <img
-                                    className="w-full h-full object-cover rounded-lg"
-                                    src={image}
-                                />
-                            </div>
+                            onClick={() => setIndexImage(images.indexOf(image))}
+                            key={index}
+                        >
+                            <img
+                                className="w-full h-full object-cover rounded-lg"
+                                src={image}
+                            />
                         </div>
                     ))}
             </div>
